773d8500edb401d1782e9571617f1560a8a4c709
carolyn_m <carolynlmadden@gmail.com>
2025-03-18 14:37:26 -0400

    Updating this list

---
+++ updating-this-list/main.py
@@ -0,0 +1,12 @@
+names = ["Samantha", "Ryan", "Cindy", "Justin", "Ashley", "Cassidy", "Lexie", "Meggan", "Lee", "Bev", "Bonnie", "Jeanne", "Anna", "Celeste", "Conrad", "Becky", "Susan", "Helen", "Alice", "Michelle"]
+dinner_list = names[:4]
+print(dinner_list)
+
+drinks_list = names[16:]
+print(drinks_list)
+
+hike_list = names[10:14]
+print(hike_list)
+
+walk_friend = names[7]
+print(walk_friend)

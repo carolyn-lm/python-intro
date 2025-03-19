493c85654210e6cb522cb59f6e420116d3239048
carolyn_m <carolynlmadden@gmail.com>
2025-03-19 11:11:10 -0400

    Updated build-a-to-do-list

--- build-a-to-do-list/main.py
+++ build-a-to-do-list/main.py
@@ -2,4 +2,12 @@ todo_list = []
 done = False
 
 print("Type 'done' at any time to exit")
+while not done:
+    new_item = input("Add an item to the todo list: ")
+    if new_item == "done":
+        done = True
+    else:
+        todo_list.append(new_item)
+
+print(todo_list)
 

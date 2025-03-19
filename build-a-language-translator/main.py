17ed7cf6502403fb719ef1730970707e772af0dd
carolyn_m <carolynlmadden@gmail.com>
2025-03-19 12:01:06 -0400

    Updated build-a-language-translator

--- build-a-language-translator/main.py
+++ build-a-language-translator/main.py
@@ -1,9 +1,20 @@
 translations = {
   "hello":"hola",
   "thank you":"gracias",
-  "sorry":"lo siento"
+  "sorry":"lo siento",
+  "work":"trabajo",
+  "today":"hoy"
 }
 
 done = False
 
 print('Type "done" at any time to exit')
+while not done:
+    word = input("Type an English word to translate: ")
+    word = word.lower()
+    if word == "done":
+        done = True
+    elif word in translations:
+        print(translations[word])
+    else:
+        print("That word is not in the dictionary")

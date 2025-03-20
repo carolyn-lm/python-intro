56a9e51691d2270a63792f9ba3cb35c98803dd5e
carolyn_m <carolynlmadden@gmail.com>
2025-03-20 14:22:13 -0400

    Update 2 to personal assistant

--- start-your-personal-assitant-class/main.py
+++ start-your-personal-assitant-class/main.py
@@ -4,6 +4,31 @@ class PersonalAssistant:
     self.contacts = {'Ann': 'Marketing Coordinator', 'Chelsea': 'Software Developer', 'Nichole': 'Sales Representative', 'Max': 'Technical Writer'}
     self.todos = []
 
+  def add_todo(self, new_item):
+    self.todos.append(new_item)
+
+  def remove_todo(self, todo_item):
+    if todo_item in self.todos:
+      # Get the todo_item index in list
+      index = self.todos.index(todo_item)
+      # pop the index for todo_item in todos list
+      self.todos.pop(index)
+    else:
+      print("Todo is not in list!")
+
+  def get_todos(self):
+    return self.todos
+  
+  def get_birthday(self, name): 
+    if name == "Samantha":
+      return "Birthday is 11/16/1996!"
+    elif name == "Ryan":
+      return "Birthday is 03/23/1999!"
+    elif name == "Cindy":
+      return "Birthday is 05/21/1976!"
+    else:
+      return "Can’t find a birthday for this person"
+
   # Complete the get_contact function code
   def get_contact(self, name):
     if name in self.contacts:
@@ -16,3 +41,7 @@ class PersonalAssistant:
 assistant = PersonalAssistant()
 # Change name to one from your contacts
 print(assistant.get_contact("Chelsea"))
+
+assistant.add_todo("Pick up groceries")
+print(assistant.get_todos())
+print(assistant.get_birthday("Samantha"))
